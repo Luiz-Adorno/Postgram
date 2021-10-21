a876ce5297b6b5fe7cd7130aaa9851848a08c5a8
Luiz Adorno <luizadorno96@gmail.com>
2021-10-21 12:09:28 -0300

    Unit test for MainViewModel implemented

---
+++ app/src/test/java/com/example/postgram/viewmodel/MainViewModelTest.kt
@@ -0,0 +1,58 @@
+package com.example.postgram.viewmodel
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.postgram.repository.Repository
+import com.example.postgram.utils.PostFactory
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
+class MainViewModelTest{
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    private val repository = mockk<Repository>()
+
+    val dispatcher = TestCoroutineDispatcher()
+
+    @Before
+    fun setup() {
+        Dispatchers.setMain(dispatcher)
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
+    @Test
+    fun `when viewmodel fetches data then it should call the repository`(){
+        val viewModel = instantiateViewModel()
+
+        val mockedList = PostFactory.post
+        every { repository.getPost() } returns MutableStateFlow(mockedList)
+
+        viewModel.getPost()
+
+        verify { repository. getPost() }
+
+    }
+
+
+    private fun instantiateViewModel(): MainViewModel{
+        return MainViewModel(repository)
+    }
+
+}
